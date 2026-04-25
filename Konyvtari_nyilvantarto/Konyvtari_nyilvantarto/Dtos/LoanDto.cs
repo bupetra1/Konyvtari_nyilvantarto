f9b1b8aa28906161a07e71b3051f92432943ba2d
Aszalós Evila <aszalosevi@gmail.com>
2026-04-25 22:51:34 +0200

    feat: BookDto, LoanDto és ReaderDto implementálása

---
+++ Konyvtari_nyilvantarto/Konyvtari_nyilvantarto/Dtos/LoanDto.cs
@@ -0,0 +1,14 @@
+namespace Konyvtari_nyilvantarto
+{
+    public class LoanDto
+    {
+        public int ReaderId {get; set;}
+        public string ReaderName { get; set; }
+        public int BookId {get; set;}
+        public string BookTitle { get; set; }
+        public string BookAuthor { get; set; }
+        public DateTime LoanDate {get; set;}
+        public DateTime DueDate {get; set;}
+        public int LateFee {get; set;}
+    }
+}
--- Konyvtari_nyilvantarto/Konyvtari_nyilvantarto/Dtos/ReaderDto.cs
+++ Konyvtari_nyilvantarto/Konyvtari_nyilvantarto/Dtos/ReaderDto.cs
@@ -2,6 +2,9 @@ namespace Konyvtari_nyilvantarto
 {
     public class ReaderDto
     {
-        
+        public int Id {get; set;}
+        public string Name {get; set;}
+        public string Address {get; set;}
+        public DateTime BirthDate {get; set;}
     }
 }
