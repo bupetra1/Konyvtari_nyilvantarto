0b5f12a9fb97cf923e455e147caf21b1296c487a
Aszalós Evila <aszalosevi@gmail.com>
2026-05-10 23:39:14 +0200

    docs: dokumentációs megjegyzések hozzáadása a CreateBookDto osztályhoz

--- Konyvtari_nyilvantarto/Share/Dtos/CreateBookDto.cs
+++ Konyvtari_nyilvantarto/Share/Dtos/CreateBookDto.cs
@@ -3,15 +3,31 @@ using Konyvtari_nyilvantarto.Validations;
 
 namespace Share.Dtos
 {
+    /// <summary>
+    /// Data transfer object containing the necessary information to register a new book.
+    /// </summary>
     public class CreateBookDto
     {
+        /// <summary>
+        /// The title of the new book.
+        /// </summary>
         [Required(ErrorMessage ="Book title is required!")]
         public string Title{get; set;} = string.Empty;
 
+        /// <summary>
+        /// The author of the new book.
+        /// </summary>
         [Required(ErrorMessage ="Book author is required!")]
         public string Author{get; set;} = string.Empty;
+
+        /// <summary>
+        /// The publisher of the new book, if available.
+        /// </summary>
         public string? Publisher{get; set;}
 
+        /// <summary>
+        /// The year the new book was published, if available. Must be non-negative and cannot exceed the current year.
+        /// </summary>
         [ValidPublicationYear]
         public int? PublicationYear {get; set;}
     }
