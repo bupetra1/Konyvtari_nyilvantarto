32f3112747cc2d21f2f5eda9009287688c3336f6
Aszalós Evila <aszalosevi@gmail.com>
2026-05-11 18:24:02 +0200

    refactor: redundáns import-ok kitörlése

--- Konyvtari_nyilvantarto/Share/Dtos/ReaderBookListDto.cs
+++ Konyvtari_nyilvantarto/Share/Dtos/ReaderBookListDto.cs
@@ -1,6 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-using Share.Validations;
-
 namespace Share.Dtos
 {
     /// <summary>
--- Konyvtari_nyilvantarto/Share/Dtos/ReaderLoanListDto.cs
+++ Konyvtari_nyilvantarto/Share/Dtos/ReaderLoanListDto.cs
@@ -1,5 +1,4 @@
-using System.ComponentModel.DataAnnotations;
-using Share.Validations;
+using Share.Logic;
 
 namespace Share.Dtos
 {
