012aee4f5771d044708e412a5fb8176e99101f49
bupetra1 <bupetra1@gmail.com>
2026-05-11 15:55:19 +0200

    feat: Loan service elkészítése

--- Konyvtari_nyilvantarto/Frontend/Services/ILoanRepository.cs
+++ Konyvtari_nyilvantarto/Frontend/Services/ILoanRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Frontend.Services
+﻿using Share.Dtos;
+
+namespace Frontend.Services
 {
     public interface ILoanRepository
     {
+        Task<List<LoanDto>> GetLoansAsync();
+        Task<bool> CreateLoanAsync(CreateLoanDto loan);
+        Task<bool> UpdateLoanAsync(int loanId, LoanDto loan);
+        Task<bool> DeleteLoanAsync(int loanId);
     }
 }
