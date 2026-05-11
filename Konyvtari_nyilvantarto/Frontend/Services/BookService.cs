d8f762ad2e4e4c47e9f091515b10d9577a55bf68
bupetra1 <bupetra1@gmail.com>
2026-05-11 13:42:31 +0200

    feat: Frontend IBookRepository implementálása a Bookservice-be.

--- Konyvtari_nyilvantarto/Frontend/Services/BookService.cs
+++ Konyvtari_nyilvantarto/Frontend/Services/BookService.cs
@@ -1,6 +1,28 @@
-﻿namespace Frontend.Services
+﻿using System.Net.Http.Json;
+using Share.Dtos;
+
+namespace Frontend.Services
 {
-    public class BookService
+    public class BookService : IBookRepository
     {
+        public Task<bool> CreateBookAsync(CreateBookDto book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteBookAsync(int bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BookDto>> GetBooksAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateBookAsync(int bookId, BookDto book)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
