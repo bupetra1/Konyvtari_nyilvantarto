7721cff34d8ff3ac71259e5387e4e43dabfb1fed
bupetra1 <bupetra1@gmail.com>
2026-05-11 13:40:41 +0200

    feat: Frontend Book CRUD műveletek felvétele interfészbe

--- Konyvtari_nyilvantarto/Frontend/Services/IBookRepository.cs
+++ Konyvtari_nyilvantarto/Frontend/Services/IBookRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace Frontend.Services
+﻿using Share.Dtos;
+namespace Frontend.Services
 {
     public interface IBookRepository
     {
+        Task<List<BookDto>> GetBooksAsync();
+        Task<bool> CreateBookAsync(CreateBookDto book);
+        Task<bool> UpdateBookAsync(int bookId, BookDto book);
+        Task<bool> DeleteBookAsync(int bookId);
     }
 }
